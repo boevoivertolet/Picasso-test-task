b46841b8951f026b2121d5a51074548c4d517694
boevoivertolet <bjha78rus@yandex.ru>
2023-10-14 11:39:07 +0300

    some styles

--- src/pages/description-page.js
+++ src/pages/description-page.js
@@ -1,6 +1,5 @@
 import React from 'react'
-import { NavLink, useParams } from 'react-router-dom'
-import { Button } from '@mui/material'
+import { useParams } from 'react-router-dom'
 import { DescriptionItem } from '../entities/descripion-item'
 
 export const DescriptionPage = ({ data }) => {
@@ -9,7 +8,7 @@ export const DescriptionPage = ({ data }) => {
       const currentPost = data && data.find((el) => el.id === currentPostId)
 
       return (
-            <div>
+            <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
                   <DescriptionItem post={currentPost} />
             </div>
       )
